568c1e64aca90776b7ec7b2910a8f5caf9b9aa2b
zulham.sy@gmail.com <zulham.sy@gmail.com>
2022-05-30 08:57:46 +0700

    changing forEach to for loop

--- index.js
+++ index.js
@@ -14,80 +14,91 @@
 const arrayObjectPegawai = require("./data-customer.json");
 
 function lakukanLooping(arrPegawai) {
-  // ! JANGAN DIMODIFIKASI
-  let dataYangAkanDilooping = arrPegawai;
+	// ! JANGAN DIMODIFIKASI
+	let dataYangAkanDilooping = arrPegawai;
 
-  /*
-    TODO 1: Buatlah sebuah variabel bernama "hasilLooping" 
-      yang berisi gabungan nama depan dan belakang dari masing masing pegawai
+	/*
+	  TODO 1: Buatlah sebuah variabel bernama "hasilLooping" 
+	    yang berisi gabungan nama depan dan belakang dari masing masing pegawai
 
-      Contoh: ["Aisyah Nirmala", "Mansur Faisal", ...]
-  */
-  let hasilLooping = [];
+	    Contoh: ["Aisyah Nirmala", "Mansur Faisal", ...]
+	*/
+	let hasilLooping = [];
 
-  /*
-    TODO 2: Buatlah sebuah variabel bernama "jumlahPria"
-      yang berisi jumlah pria dari masing masing pegawai
-  */
-  let jumlahPria = 0;
+	/*
+	  TODO 2: Buatlah sebuah variabel bernama "jumlahPria"
+	    yang berisi jumlah pria dari masing masing pegawai
+	*/
+	let jumlahPria = 0;
 
-  /*
-    TODO 3: Buatlah sebuah variabel bernama "jumlahWanita"
-      yang berisi jumlah wanita dari masing masing pegawai
-  */
-  let jumlahWanita = 0;
+	/*
+	  TODO 3: Buatlah sebuah variabel bernama "jumlahWanita"
+	    yang berisi jumlah wanita dari masing masing pegawai
+	*/
+	let jumlahWanita = 0;
 
-  /*
-    TODO 4: Buatlah sebuah variabel bernama "komentar"
-      yang akan mengomentari apakah lebih banyak Pria atau Wanita
-      
-      Apabila Pria lebih banyak dari Wanita, maka komentar akan berisi:
-        "Jumlah Pria lebih banyak dari Wanita"
-      Apabila Wanita lebih banyak dari Pria, maka komentar akan berisi:
-        "Jumlah Wanita lebih banyak dari Pria"
-      Apabila imbang, maka komentar akan berisi:
-        "Jumlah Pria dan Wanita berimbang"
-  */
-  let komentar = null;
-  
-  dataYangAkanDilooping.forEach((el) => {
-  	// TODO 1
-  	hasilLooping.push(el.namaDepan + ' ' + el.namaBelakang)
-  	// TODO 2, 3
-  	if(el.jenisKelamin == 'M') {
-  		jumlahPria++
-  	} else {
-  		jumlahWanita++
-  	}
-  })
-  // TODO 4
-  if(jumlahPria > jumlahWanita) {
-  	komentar = 'Jumlah Pria lebih banyak dari Wanita'
-  }
-  if(jumlahWanita > jumlahPria) {
-  	komentar = 'Jumlah Wanita lebih banyak dari Pria'
-  }
-  if(jumlahPria == jumlahWanita) {
-  	komentar = 'Jumlah Pria dan Wanita berimbang'
-  }
+	/*
+	  TODO 4: Buatlah sebuah variabel bernama "komentar"
+	    yang akan mengomentari apakah lebih banyak Pria atau Wanita
+	    
+	    Apabila Pria lebih banyak dari Wanita, maka komentar akan berisi:
+	      "Jumlah Pria lebih banyak dari Wanita"
+	    Apabila Wanita lebih banyak dari Pria, maka komentar akan berisi:
+	      "Jumlah Wanita lebih banyak dari Pria"
+	    Apabila imbang, maka komentar akan berisi:
+	      "Jumlah Pria dan Wanita berimbang"
+	*/
+	let komentar = null;
+	for (let i = 0; i < dataYangAkanDilooping.length; i++) {
+		// TODO 1
+		hasilLooping.push(dataYangAkanDilooping[i].namaDepan + ' ' + dataYangAkanDilooping[i].namaBelakang)
+		// TODO 2, 3
+		if (dataYangAkanDilooping[i].jenisKelamin == 'M') {
+			jumlahPria++
+		} else {
+			jumlahWanita++
+		}
+	}
+	/*
+	dataYangAkanDilooping.forEach((el) => {
+		// TODO 1
+		hasilLooping.push(el.namaDepan + ' ' + el.namaBelakang)
+		// TODO 2, 3
+		if (el.jenisKelamin == 'M') {
+			jumlahPria++
+		} else {
+			jumlahWanita++
+		}
+	})
+	*/
+	// TODO 4
+	if (jumlahPria > jumlahWanita) {
+		komentar = 'Jumlah Pria lebih banyak dari Wanita'
+	}
+	if (jumlahWanita > jumlahPria) {
+		komentar = 'Jumlah Wanita lebih banyak dari Pria'
+	}
+	if (jumlahPria == jumlahWanita) {
+		komentar = 'Jumlah Pria dan Wanita berimbang'
+	}
 
-  // ! JANGAN DIMODIFIKASI
-  return {
-    hasilLooping,
-    jumlahPria,
-    jumlahWanita,
-    komentar,
-  };
+	// ! JANGAN DIMODIFIKASI
+	return {
+		hasilLooping,
+		jumlahPria,
+		jumlahWanita,
+		komentar,
+	};
 }
 
 function main(data) {
-  const hasil = lakukanLooping(data || arrayObjectPegawai);
+	const hasil = lakukanLooping(data || arrayObjectPegawai);
 
-  console.log(hasil.hasilLooping);
-  console.log(hasil.jumlahPria);
-  console.log(hasil.jumlahWanita);
+	console.log(hasil.hasilLooping);
+	console.log(hasil.jumlahPria);
+	console.log(hasil.jumlahWanita);
 
-  return hasil;
+	return hasil;
 }
 
 main(arrayObjectPegawai);
